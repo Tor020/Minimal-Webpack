92d5f400688a7e5f940f1635d1084a2e269c868f
= <=>
2018-02-11 22:53:21 -0500

    Works which is gr8 but it needs nodemon for live reload or something for live or 'hot' reload etc

--- src/main.js
+++ src/main.js
@@ -1,10 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Counter from './Counter';
+import check from './Counter'
 
-document.addEventListener('DOMContentLoaded', function() {
-  ReactDOM.render(
-    React.createElement(Counter),
-    document.getElementById('mount')
-  );
-});
+console.log('on');
+check();
